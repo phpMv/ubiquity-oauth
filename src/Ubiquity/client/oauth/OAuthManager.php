c32a9b275ddcab87bbd61652638be25ebeca6dd0
jcheron <myaddressmail@gmail.com>
2020-04-13 14:56:57 +0200

    Update OAuthManager.php
    see https://github.com/phpMv/ubiquity/issues/112

--- src/Ubiquity/client/oauth/OAuthManager.php
+++ src/Ubiquity/client/oauth/OAuthManager.php
@@ -14,26 +14,63 @@ use Hybridauth\Adapter\AdapterInterface;
  */
 class OAuthManager {
 
+	/**
+	 *
+	 * @var array
+	 */
 	private static $config;
 
+	/**
+	 *
+	 * @var Hybridauth
+	 */
 	private static $oauth;
 
+	/**
+	 *
+	 * @var \Hybridauth\Adapter\AdapterInterface[]
+	 */
 	private static $adapters;
 
 	/**
 	 * Creates Hybridauth object and load config
+	 *
+	 * @param ?string $callbackUrl
 	 */
-	public static function start(): void {
+	public static function start(?string $callbackUrl = null): void {
 		self::$config = self::getConfig();
+		if (isset($callbackUrl)) {
+			self::$config['callback'] = $callbackUrl;
+		}
 		self::$oauth = new Hybridauth(self::$config);
 	}
 
+	/**
+	 *
+	 * @param string $name
+	 * @param string $callbackUrl
+	 * @return \Hybridauth\Adapter\AdapterInterface
+	 */
+	public static function startAdapter(string $name, ?string $callbackUrl = null) {
+		self::start($callbackUrl);
+		return self::authenticate($name);
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
 	public static function getConfig(): array {
 		$file = \ROOT . \DS . 'config' . \DS . 'oauth.php';
 		if (file_exists($file)) {
 			return self::$config = include $file;
 		}
-		return [];
+		return [
+			'callback' => '',
+			'providers' => [],
+			'debug_mode' => false,
+			'debug_file' => './oauth.log'
+		];
 	}
 
 	/**
@@ -41,7 +78,7 @@ class OAuthManager {
 	 * @param string $name
 	 * @return \Hybridauth\Adapter\AdapterInterface
 	 */
-	public static function authenticate($name): AdapterInterface {
+	public static function authenticate(string $name): AdapterInterface {
 		return self::$adapters[$name] = self::$oauth->authenticate($name);
 	}
 
@@ -50,16 +87,39 @@ class OAuthManager {
 	 * @param string $name
 	 * @return \Hybridauth\Adapter\AdapterInterface
 	 */
-	public static function getAdapter($name): AdapterInterface {
+	public static function getAdapter(string $name): AdapterInterface {
 		if (! isset(self::$adapters[$name])) {
 			return self::authenticate($name);
 		}
 		return self::$adapters[$name];
 	}
 
-	public static function disconnect($name): void {
+	/**
+	 * Disconnect from $name provider and clears all access token
+	 *
+	 * @param string $name
+	 *        	The provider name
+	 */
+	public static function disconnect(string $name): void {
 		$adapter = self::getAdapter($name);
 		$adapter->disconnect();
+	}
+
+	/**
+	 *
+	 * @param string $name
+	 * @return array
+	 */
+	public static function getProviderConfig(string $name): array {
+		return (self::$config['providers'][$name]) ?? ([
+			'enabled' => false,
+			'force' => false,
+			'keys' => [
+				'id' => '',
+				'secret' => ''
+			],
+			'scope' => ''
+		]);
 	}
 }
 
